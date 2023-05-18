d1546bd1639adc0154f958594040718e0b995e0a
Kmroiosn <multution@outlook.com>
2023-05-18 17:44:58 +0800

    Calendar: 实现选中日期激活

--- my-implementation/calendar_app/src/app/store.js
+++ my-implementation/calendar_app/src/app/store.js
@@ -6,5 +6,13 @@ export const store = {
         // reactive() 将产生一个由 Vue 记录的对象，使其可以在
         // 多个组件间调用而保持一致性
         data: reactive(seedData)
+    },
+    getActiveDay() {
+        return this.state.data.find((day) => day.active);
+    },
+    setActiveDay(dayId) {
+        this.state.data.map((day) => {
+            day.active = day.id === dayId ? true : false;
+        });
     }
 }
